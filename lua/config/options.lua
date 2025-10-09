1bc07beeaaf0976ebbeec4b3256817eba12bca5a
digos <diogobissolimoreno@gmail.com>
2025-10-09 20:43:36 -0300

    feat: adicionando quebra de linha virtual

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -30,3 +30,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern="*",
   command="%s/\\s\\+$//e",
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "gitcommit" },
+  callback = function()
+   vim.opt_local.wrap = true
+   vim.opt_local.linebreak = true
+   vim.opt_local.showbreak = "↪ "
+  end
+})
