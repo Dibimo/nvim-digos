3a424cbb78ddc8820fdace9167c358e8c030d91d
digos <diogobissolimoreno@gmail.com>
2025-10-09 21:17:08 -0300

    refactor: atualizando LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
     end,
   },
 
-  -- LSP Config (SEM mason-lspconfig)
+  -- LSP Config (NOVA API do Neovim 0.11+)
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
@@ -28,41 +28,49 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- Configurar capabilities para autocompletion
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      
-      -- Tentar carregar cmp_nvim_lsp se estiver disponível
+
       local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
       if has_cmp then
         capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
       end
 
-      -- Função on_attach
-      local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, silent = true }
-
-        -- LSP Keymaps
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, opts)
-        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-      end
-
-      -- Configuração manual dos servers (SEM automatização)
-      local lspconfig = require("lspconfig")
-
-      -- Lua Language Server
-      lspconfig.lua_ls.setup({
+      vim.lsp.config('*', {
         capabilities = capabilities,
-        on_attach = on_attach,
+        root_markers = { '.git' }  -- fallback padrão
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+        callback = function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          local opts = { buffer = bufnr, silent = true }
+
+          -- Keymaps do LSP
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>lf", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+          if client.supports_method('textDocument/inlayHint') then
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          end
+        end,
+      })
+
+      vim.lsp.config('lua_ls', {
+        cmd = { 'lua-language-server' },
+        filetypes = { 'lua' },
+        root_markers = { '.luarc.json', '.luarc.jsonc', '.stylua.toml', '.git' },
         settings = {
           Lua = {
             runtime = { version = "LuaJIT" },
@@ -75,46 +83,110 @@ return {
           },
         },
       })
-      --emmet
-      lspconfig.emmet_language_server.setup({})
 
-      -- Python
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- Emmet Language Server
+      vim.lsp.config('emmet_language_server', {
+        cmd = { 'emmet-language-server', '--stdio' },
+        filetypes = { 'html', 'css', 'javascriptreact', 'typescriptreact' },
+        root_markers = { '.git', 'package.json' },
+      })
+
+      -- Python (Pyright)
+      vim.lsp.config('pyright', {
+        cmd = { 'pyright-langserver', '--stdio' },
+        filetypes = { 'python' },
+        root_markers = {
+          'pyproject.toml',
+          'setup.py',
+          'requirements.txt',
+          '.git'
+        },
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            }
+          }
+        }
       })
 
       -- TypeScript/JavaScript
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.config('ts_ls', {
+        cmd = { 'typescript-language-server', '--stdio' },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'typescript',
+          'typescriptreact'
+        },
+        root_markers = {
+          'tsconfig.json',
+          'jsconfig.json',
+          'package.json',
+          '.git'
+        },
       })
 
-      -- HTML
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- HTML (precisa de snippet support)
+      local html_capabilities = vim.deepcopy(capabilities)
+      html_capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      vim.lsp.config('html', {
+        cmd = { 'vscode-html-language-server', '--stdio' },
+        filetypes = { 'html' },
+        root_markers = { 'package.json', '.git' },
+        capabilities = html_capabilities,
+        init_options = {
+          provideFormatter = true,
+        },
       })
 
-      -- CSS
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- CSS (precisa de snippet support)
+      vim.lsp.config('cssls', {
+        cmd = { 'vscode-css-language-server', '--stdio' },
+        filetypes = { 'css', 'scss', 'less' },
+        root_markers = { 'package.json', '.git' },
+        capabilities = html_capabilities,
+        settings = {
+          css = { validate = true },
+          scss = { validate = true },
+          less = { validate = true },
+        },
       })
 
-      -- JSON
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- JSON (precisa de snippet support)
+      vim.lsp.config('jsonls', {
+        cmd = { 'vscode-json-language-server', '--stdio' },
+        filetypes = { 'json', 'jsonc' },
+        root_markers = { 'package.json', '.git' },
+        capabilities = html_capabilities,
+        settings = {
+          json = {
+            validate = { enable = true },
+            format = { enable = true },
+          },
+        },
       })
 
       -- Bash
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.config('bashls', {
+        cmd = { 'bash-language-server', 'start' },
+        filetypes = { 'sh', 'bash' },
+        root_markers = { '.git' },
       })
 
-      -- Configuração dos diagnósticos
+      vim.lsp.enable({
+        'lua_ls',
+        'emmet_language_server',
+        'pyright',
+        'ts_ls',
+        'html',
+        'cssls',
+        'jsonls',
+        'bashls',
+      })
+
       vim.diagnostic.config({
         virtual_text = {
           prefix = "●",
@@ -126,12 +198,16 @@ return {
           focusable = false,
           style = "minimal",
           border = "rounded",
-          source = "always",
         },
       })
 
       -- Ícones dos diagnósticos
-      local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+      local signs = {
+        Error = " ",
+        Warn = " ",
+        Hint = "󰌶 ",
+        Info = " "
+      }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
