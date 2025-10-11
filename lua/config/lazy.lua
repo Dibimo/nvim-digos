5762bc50877b35fb65cf8c8007aebce526f10fc1
digos <diogobissolimoreno@gmail.com>
2025-10-11 20:17:31 -0300

    refactor: configurando lazy.nvim para encontrar sub modulos de plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,4 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.ui" }
+  },
+  import = "plugins.**"
+})
