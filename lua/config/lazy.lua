283e116f4d6b7ce6d17191d223fc9ea2555b3cf8
digos <diogobissolimoreno@gmail.com>
2025-10-11 20:26:09 -0300

    feat: sub modulo lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "plugins.ui" },
     { import = "plugins.editor" },
     { import = "plugins.coding" },
+    { import = "plugins.lsp" },
   },
   import = "plugins.**"
 })
