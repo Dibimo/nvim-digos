868e88d66ad39cd608bb3f7bab58c13968d9dcb7
digos <diogobissolimoreno@gmail.com>
2025-10-11 20:21:22 -0300

    refactor: criando sub modulo para plugins de editor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,9 @@
 return {
   {
-    'numToStr/Comment.nvim',
-    opts = {
-      -- add any options here
-    }
-  },
-  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  },
-  --
-  -- temas
- --fim temas
-  --
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
