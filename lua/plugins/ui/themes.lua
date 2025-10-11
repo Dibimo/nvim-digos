a62d8c488a8eee7b15a4ebb97a4f21785c9ce03f
digos <diogobissolimoreno@gmail.com>
2025-10-11 20:30:15 -0300

    refactor: passando themes para ui

---
+++ lua/plugins/ui/themes.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "shaunsingh/nord.nvim",
+    options = {
+      theme = 'nord'
+    }
+
+  },
+  { "EdenEast/nightfox.nvim" }
+}
+
