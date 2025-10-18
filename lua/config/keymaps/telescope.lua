a736b623d61067d321c00e39f4b7fcd80da79191
digos <diogobissolimoreno@gmail.com>
2025-10-18 19:55:34 -0300

    feat: comando para busca na outline

--- lua/config/keymaps/telescope.lua
+++ lua/config/keymaps/telescope.lua
@@ -5,4 +5,5 @@ keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files"
 keymap("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find word" })
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help" })
+keymap("n", "<leader>fo", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find help" })
 
