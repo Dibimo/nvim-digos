1cd1cb2a85cd17c1a30d1d3531a71db1a8008b97
Digos <diogobissolimoreno@gmail.com>
2025-12-19 10:05:50 -0300

    refactor: neo-tree com float por padrão

--- lua/plugins/tools/neo-tree.lua
+++ lua/plugins/tools/neo-tree.lua
@@ -8,7 +8,11 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      require("neo-tree").setup({})
+      require("neo-tree").setup({
+        window = {
+          position = "float"
+        }
+      })
     end,
   },
 
