3723b145f6725d5ee386645291093e7ef8b2de1a
Digos <diogobissolimoreno@gmail.com>
2025-12-19 10:05:20 -0300

    refactor: ajustes para copiar e colar diretamente do sistema

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,3 +50,29 @@ vim.api.nvim_create_autocmd("FileType", {
    vim.opt_local.showbreak = "↪ "
   end
 })
+
+opt.clipboard = "unnamedplus"
+
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.neovide_opacity_point or 0.8))
+end
+-- Set transparency and background color (title bar color)
+-- vim.g.neovide_opacity = 0.88
+-- vim.g.transparency = 0.70
+
+
+
+-- local change_transparency = function(delta)
+--   vim.g.neovide_opacity_point = vim.g.neovide_opacity_point + delta
+--   vim.g.neovide_background_color = "#0f1117" .. alpha()
+-- end
+
+--
+--
+-- vim.keymap.set({ "n", "v", "o" }, "<D-]>", function()
+--   change_transparency(0.01)
+-- end)
+-- vim.keymap.set({ "n", "v", "o" }, "<D-[>", function()
+--   change_transparency(-0.01)
+-- end)
