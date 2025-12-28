7f99d4e44cfdf1b861b77c8f2e3ef2ac3687544e
Digos <diogobissolimoreno@gmail.com>
2025-12-28 11:37:16 -0300

    fix: resolvendo problema de incompatibilidade com git sings

--- lua/custom/relative-keys.lua
+++ lua/custom/relative-keys.lua
@@ -5,15 +5,18 @@ function M.get_line_number()
   local current = vim.fn.line('.')
   local relative = line - current
 
+  local number_part = ""
+
   if relative == 0 then
-    return string.format('%4d', line)
+    number_part = string.format('%4d', line)
   else
     local abs_relative = math.abs(relative)
     local key = relative > 0 and "j" or "k"
     local hl = relative > 0 and "DiagnosticInfo" or "DiagnosticHint"
 
-    return string.format("%%#%s#%4d%s", hl, abs_relative, key)
+    number_part = string.format("%%#%s#%4d%s", hl, abs_relative, key)
   end
+  return "%s" .. number_part .. " "
 end
 
 function M.enable()
